cabea233ccb3ec0ffb18a43129ebafaa7d121fcb
k-shibta <k-shibata@chunousinrin.or.jp>
2024-04-22 18:15:40 +0900

    202404221815

---
+++ app/Http/Controllers/BillController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BillController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+    public function bill()
+    {
+        return view('bill.bill');
+    }
+}
