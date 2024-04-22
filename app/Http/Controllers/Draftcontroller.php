c03e1606218be74964c9415cfdc01e283b18806e
k-shibta <k-shibata@chunousinrin.or.jp>
2024-04-22 12:21:14 +0900

    202404221221

--- app/Http/Controllers/Draftcontroller.php
+++ app/Http/Controllers/Draftcontroller.php
@@ -4,57 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Draftcontroller extends Controller
+class DraftController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
+     * Handle the incoming request.
      */
-    public function __construct()
+    public function __invoke(Request $request)
     {
-        $this->middleware('auth');
+        //
     }
-
-
     public function draft()
     {
         return view('draft.draft');
-    }
-    public function draft_input()
-    {
-        return view('draft.draft_input');
-    }
-    public function draft_restate()
-    {
-        return view('draft.draft_restate');
-    }
-    public function draft_conf()
-    {
-        return view('draft.draft_conf');
-    }
-    public function draft_preview()
-    {
-        return view('draft.draft_preview');
-    }
-    public function draft_repreview()
-    {
-        return view('draft.draft_repreview');
-    }
-    public function draft_submit()
-    {
-        return view('draft.draft_submit');
-    }
-    public function draft_delete()
-    {
-        return view('draft.draft_delete');
-    }
-    public function draft_com_submit()
-    {
-        return view('draft.draft_com_submit');
-    }
-    public function wareki()
-    {
-        return view('edt.wareki');
     }
 }
