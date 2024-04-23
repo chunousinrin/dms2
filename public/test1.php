5ecd2903b69811cd174be9a8fb87627977295346
k-shibta <k-shibata@chunousinrin.or.jp>
2024-04-23 13:16:08 +0900

    202404231316

--- public/test1.php
+++ public/test1.php
@@ -1,14 +1,6 @@
-<style>
-    body {
-        background-color: rgba(0, 0, 0, 0.5);
-    }
-</style>
-
 <?php
-$dbh = new PDO('mysql:host=localhost;dbname=forest_union;charset=utf8', 'root', '');
-$sql = "SELECT * FROM union_members WHERE 脱退年月日 is null";
-$stmt = $dbh->query($sql);
-while ($result = $stmt->fetch(PDO::FETCH_BOTH)) : ?>
-    <div><?= $result['組合員番号'] ?>　<?= $result['氏名1'] ?></div>
 
-<?php endwhile ?>
+$test = DB::table('classication')
+    ->get();
+
+var_dump($test);
