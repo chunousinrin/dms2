c1ccc5dc8fde062bb2da4c53a01c5a8882b0df9d
k-shibta <k-shibata@chunousinrin.or.jp>
2024-06-20 18:19:25 +0900

    202406201819

--- resources/views/attendance/worker/wat3.blade.php
+++ resources/views/attendance/worker/wat3.blade.php
@@ -35,7 +35,8 @@ $wgl_st = $dbh->query($wgl_sql); ?>
                                 <input type="hidden" name="workerid<?= $opt2; ?>" id="workerid<?= $opt2; ?>" value="<?= $result['WorkerNameID'] ?>" hidden>
                                 <label class="col-form-label"><?= $result['WorkerName'] ?></label>
                                 <input type="text" name="workername<?= $opt2; ?>" id="workername<?= $opt2; ?>" value="<?= $result['WorkerName'] ?>" hidden>
-                                <?php $wnicount = $wnicount + $wnic['cnt']; ?>
+                                <?php $wnicount = $wnicount + $wnic['cnt'];
+                                echo $wnicount ?>
                             </td>
                             <td class="form-group col-2 text-center rb">
                                 <input type="radio" name="shukkin<?= $opt2; ?>" id="shukkin<?= $opt2; ?>" class="form-check-input border-success" style="width:1.5em;height:1.5em;" value="1">
