5b9d8b1e99da02d8affd5961b0e8a8ad4eb14033
k-shibta <k-shibata@chunousinrin.or.jp>
2024-06-24 09:37:29 +0900

    202406240937

--- public/test1.php
+++ public/test1.php
@@ -74,7 +74,7 @@ if (!empty($_POST['member'])) {
     $calstmt = $dbh->query($calsql);
     while ($result = $calstmt->fetch(PDO::FETCH_BOTH)) : ?>
         <div style="display: flex;">
-            <div><?= $result['CalDate'] . " (" . $week["1"('w')] . ")" ?></div>
+            <div><?= $result['CalDate'] . " (" . $result['wd'] . ")" ?></div>
             <div style="padding: 0 1em;"><?= $result['WorkerName'] ?></div>
             <div style="padding: 0 1em;"><?= $result['watID'] ?></div>
             <div style="padding: 0 1em;"><?= $result['AttendanceType'] ?></div>
