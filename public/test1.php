dbdba0bd62b4bb0ea0ee4ce5eeb27c04150a7505
k-shibta <k-shibata@chunousinrin.or.jp>
2024-06-24 09:20:06 +0900

    202406240920

--- public/test1.php
+++ public/test1.php
@@ -80,10 +80,9 @@ if (!empty($_POST['member'])) {
 
     $calsql = "SELECT * FROM( SELECT * FROM worker_attendance_view WHERE WorkerNameID = " . $_POST['member'] . ") AS atview RIGHT JOIN cal_test ON atview.AttendanceDay=cal_test.CalDate;";
     $calstmt = $dbh->query($calsql);
-    while ($result = $calstmt->fetch(PDO::FETCH_BOTH)) :
-        $daylist = date('Y-m-d', strtotime($result['CalDate'])) ?>
+    while ($result = $calstmt->fetch(PDO::FETCH_BOTH)) : ?>
         <div style="display: flex;">
-            <div><?= $result['CalDate'] . " (" . $week[$daylist("w")] . ")" ?></div>
+            <div><?= $result['CalDate'] . " (" . $week[$result['CalDate']("w")] . ")" ?></div>
             <div style="padding: 0 1em;"><?= $result['WorkerName'] ?></div>
             <div style="padding: 0 1em;"><?= $result['watID'] ?></div>
             <div style="padding: 0 1em;"><?= $result['AttendanceType'] ?></div>
