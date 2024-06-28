ea33f7b524769b156008ebfb8823f5ba9e3e5c7a
k-shibta <k-shibata@chunousinrin.or.jp>
2024-06-28 10:36:28 +0900

    202406281036

--- resources/views/home/worker_attendance_check.blade.php
+++ resources/views/home/worker_attendance_check.blade.php
@@ -1,22 +1,22 @@
-<form action="" method="post">
-    @csrf
-    <input type="date" name="kyo" id="kyo" class="datepicker" onchange="submit();">
-</form>
-<?php
-
-if (empty($_POST['kyo'])) {
-    $kyo = date("Y-m-d");
-    $open = "";
-} else {
-    $kyo = ($_POST['kyo']);
-    $open = "open";
-}
-$dbh = new PDO('mysql:host=localhost;dbname=' . env('DB_DATABASE') . ';charset=utf8', env('DB_USERNAME'), env('DB_PASSWORD'));
-$atnowsql = "SELECT wgm.kyo AS wgmAttendanceDay, wgm.WorkerNameID AS wgmWNI, wgm.WorkerName AS wgmWN, worker_attendance_view.* FROM ( SELECT *, DATE(NOW()) AS kyo FROM worker_group_member) AS wgm LEFT JOIN worker_attendance_view ON wgm.kyo = worker_attendance_view.AttendanceDay AND wgm.WorkerNameID = worker_attendance_view.WorkerNameID WHERE worker_attendance_view.NumberOfDaysWorked is null;";
-$atnowstmt = $dbh->query($atnowsql);
-?>
-
 <div class="shadow bg-white p-3 mb-2">
+    <form action="" method="post">
+        @csrf
+        <input type="text" name="kyo" id="kyo" class="datepicker" onchange="submit();">
+    </form>
+    <?php
+
+    if (empty($_POST['kyo'])) {
+        $kyo = date("Y-m-d");
+        $open = "";
+    } else {
+        $kyo = ($_POST['kyo']);
+        $open = "open";
+    }
+    $dbh = new PDO('mysql:host=localhost;dbname=' . env('DB_DATABASE') . ';charset=utf8', env('DB_USERNAME'), env('DB_PASSWORD'));
+    $atnowsql = "SELECT wgm.kyo AS wgmAttendanceDay, wgm.WorkerNameID AS wgmWNI, wgm.WorkerName AS wgmWN, worker_attendance_view.* FROM ( SELECT *, DATE(NOW()) AS kyo FROM worker_group_member) AS wgm LEFT JOIN worker_attendance_view ON wgm.kyo = worker_attendance_view.AttendanceDay AND wgm.WorkerNameID = worker_attendance_view.WorkerNameID WHERE worker_attendance_view.NumberOfDaysWorked is null;";
+    $atnowstmt = $dbh->query($atnowsql);
+    ?>
+
     <details <?= " " . $open ?>>
         <summary>作業班出退勤未入力者</summary>
         <table style="margin: 0 auto;">
