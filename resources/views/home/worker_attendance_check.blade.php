195194126e08451faec56ee56ba4b0d0801ac135
k-shibta <k-shibata@chunousinrin.or.jp>
2024-07-03 10:59:03 +0900

    202407031058

--- resources/views/home/worker_attendance_check.blade.php
+++ resources/views/home/worker_attendance_check.blade.php
@@ -1,9 +1,10 @@
 <div class="bg-white mb-3 p-2" style="box-shadow: 5px 5px 5px -5px #464646;">
-    <div class="text-center" style="position: relative;">作業班出退勤
-        <button class="btn btn-sm btn-info py-0 rounded-0" style="position: absolute; top:0;right:0;">印刷</button>
-    </div>
     <form action="https://cf444722.cloudfree.jp/worker/wat6?kyo=<?= $_GET['kyo'] ?? null ?>" method="get" target="wath">
         @csrf
+        <div class="text-center" style="position: relative;">作業班出退勤
+            <button class="btn btn-sm btn-info py-0 rounded-0" style="position: absolute; top:0;right:0;" formaction="https://cf444722.cloudfree.jp/worker/print" formtarget="_new">印刷</button>
+        </div>
+
         <input type="text" name="kyo" id="kyo" class="form-control rounded-0 datepicker" placeholder="日付を選択" onchange="submit();" value="<?= $_GET['kyo'] ?? null ?>">
     </form>
 
