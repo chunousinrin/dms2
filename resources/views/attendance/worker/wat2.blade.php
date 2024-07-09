388834d3731001460859253b8e5cab3e09dfb0dd
k-shibta <k-shibata@chunousinrin.or.jp>
2024-07-09 09:34:23 +0900

    202407090934

--- resources/views/attendance/worker/wat2.blade.php
+++ resources/views/attendance/worker/wat2.blade.php
@@ -3,7 +3,21 @@ $wg_sql = "SELECT * FROM worker_group ORDER BY WorkerGroupID ASC";
 $wg_st = $dbh->query($wg_sql); ?>
 <form action="" method="post" name="wat2">
     @csrf
-    <input type="text" id="shukkinbi" name="shukkinbi" class="fs form-control rounded-0 datepicker text-center" style="padding:1.5em" placeholder="出勤日を選択 &#xf073;" value="" required onchange="datecheck()" readonly="readonly" autofocus>
+    <?php
+    if (empty($_GET['ipt'])) {
+        $dtc = ' onchange="datecheck();"';
+        $ipt = "";
+    } elseif (empty($_POST['ipt'])) {
+        if ($_GET['ipt'] == "admin") {
+            $dtc = "";
+            $ipt = "admin";
+        }
+    } elseif ($_GET['ipt'] == "admin") {
+        $dtc = "";
+        $ipt = "";
+    }
+    ?>
+    <input type="text" id="shukkinbi" name="shukkinbi" class="fs form-control rounded-0 datepicker text-center" style="padding:1.5em" placeholder="出勤日を選択 &#xf073;" value="" required <?= $dtc ?> readonly="readonly" autofocus>
     <section>
         <?php $opn = 0;
         while ($result = $wg_st->fetch(PDO::FETCH_BOTH)) :
@@ -16,3 +30,9 @@ $wg_st = $dbh->query($wg_sql); ?>
 </form>
 
 <a style="text-decoration: none;border: 0.3em solid rgba(32, 178, 170, 1);background-color: rgba(32, 178, 170, 0.5);padding:0.5em;position:absolute;top:1%;right:1%;color:#000;text-align:center;" href="../worker/print"><span>出勤表<br>印刷</span></a>
+
+<?php
+if ($ipt == "admin") : ?>
+    <div style="position: absolute; bottom:0;left:0;background-color:chocolate;color:white;width:100%;text-align:center;">管理者モード</div>
+
+<?php endif ?>
--- resources/views/home/worker_attendance_check.blade.php
+++ resources/views/home/worker_attendance_check.blade.php
@@ -1,10 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+
+$user = Auth::user();
+?>
 <div class="bg-white mb-3 p-2" style="box-shadow: 5px 5px 5px -5px #464646;">
     <form action="https://cf444722.cloudfree.jp/worker/wat6?kyo=<?= $_GET['kyo'] ?? null ?>" method="get" target="wath">
         @csrf
-        <div class="text-center" style="position: relative;">作業班出退勤
-            <button class="btn btn-sm btn-info py-0 rounded-0" style="position: absolute; top:0;right:0;" formaction="https://cf444722.cloudfree.jp/worker/print" formtarget="_new">印刷</button>
+        <div class="d-flex">
+            <div class="flex-grow-1 text-center">作業班出退勤</div>
+            <?php
+            if ($user['authtype'] == 1) : ?>
+                <button class="btn btn-sm btn-info rounded-0 py-0" formaction="/worker" formtarget="_new" name="ipt" value="admin">入力</button>
+            <?php endif ?>
+            <button class="btn btn-sm btn-info rounded-0 py-0" formaction="/worker/print" formtarget="_new" name="ipt" value="prnt">印刷</button>
         </div>
-
         <input type="text" name="kyo" id="kyo" class="form-control rounded-0 datepicker" placeholder="日付を選択" onchange="submit();" value="<?= $_GET['kyo'] ?? null ?>">
     </form>
 
