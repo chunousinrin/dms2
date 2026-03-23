e8f5447d2247d705232e7d7fdb8fa6edc17dce7b
k-shibta <k-shibata@chunousinrin.or.jp>
2026-03-23 12:33:19 +0900

    202603231233

--- routes/api.php
+++ routes/api.php
@@ -61,3 +61,19 @@ Route::get('/create-new-menu', function () {
 
     return "作成失敗 (" . $response->status() . "): " . $response->body();
 });
+
+
+Route::get('/upload-to-new', function () {
+    $token = App\Services\LwApiService::getAccessToken();
+    $newId = "rm-2205960";
+    $imagePath = public_path('images/menu.png');
+
+    $response = Http::withToken($token)
+        ->attach('file', file_get_contents($imagePath), 'menu.png')
+        ->post("https://www.worksapis.com/v1.0/bots/6811630/richmenus/{$newId}/image");
+
+    if ($response->successful()) {
+        return "【ついに成功！！】画像が乗りました。次は有効化です。 ID: " . $newId;
+    }
+    return "まだダメでした: " . $response->body();
+});
