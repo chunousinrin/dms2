b544f5f102d9c743af6b7a1d8b8c5ffda07b6dd0
k-shibta <k-shibata@chunousinrin.or.jp>
2026-03-24 13:18:31 +0900

    202603241318

--- app/Services/LwApiService.php
+++ app/Services/LwApiService.php
@@ -55,21 +55,38 @@ class LwApiService
     public static function sendAttendanceSelection($userId)
     {
         $token = self::getAccessToken();
+        $botNo = "6811673";
+        $url = "https://www.worksapis.com/v1.0/bots/{$botNo}/users/{$userId}/messages";
 
-        $token = self::getAccessToken();
-        $botId = "6811673";
+        $options = [
+            ['label' => '1.0 出勤',      'val' => '1.0/出勤'],
+            ['label' => '1.0 有給',      'val' => '1.0/有給'],
+            ['label' => '1.0 特休',      'val' => '1.0/特休'],
+            ['label' => '1.0 出勤-有給', 'val' => '1.0/出勤-有給'],
+            ['label' => '0.5 出勤-欠勤', 'val' => '0.5/出勤-欠勤'],
+            //['label' => '0.5 有給-欠勤', 'val' => '0.5/有給-欠勤'],
+            //['label' => '0.0 欠勤',      'val' => '0.0/欠勤'],
+        ];
 
-        // ✅ URLの末尾に「s」をつけないパターンも試す価値あり
-        $url = "https://www.worksapis.com/v2/bot/{$botId}";
+        $items = array_map(function ($opt) {
+            return [
+                "action" => [
+                    "type" => "message",
+                    "label" => $opt['label'],
+                    "text" => "【打刻】" . $opt['val']
+                ]
+            ];
+        }, $options);
 
-        $response = Http::withHeaders([
-            'Authorization' => 'Bearer ' . $token,
-        ])->get($url);
-
-        \Log::info("DEBUG Token Status: " . $response->status());
-        \Log::info("DEBUG Token Body: " . $response->body());
-
-        return $response->status();
+        return Http::withToken($token)->post($url, [
+            "content" => [
+                "type" => "text",
+                "text" => "本日の出勤内訳を選択してください。"
+            ],
+            "quickReply" => [
+                "items" => $items
+            ]
+        ]);
     }
 
 
